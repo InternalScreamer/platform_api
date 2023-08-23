e9d3fc1279e0e23534ba95ace69e0c9fa4087bd7
InternalScreamer <nchad95@gmail.com>
2023-08-23 02:13:55 -0700

    Changed it to use freertos and esp libraries. Zepphyr didnt cut it.

---
+++ main/main.c
@@ -0,0 +1,209 @@
+/* Captive Portal Example
+
+    This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+    Unless required by applicable law or agreed to in writing, this
+    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+    CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#include <sys/param.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_mac.h"
+
+#include "nvs_flash.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_wifi.h"
+#include "esp_netif.h"
+#include "lwip/inet.h"
+
+#include "esp_http_server.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+extern const char root_start[] asm("_binary_root_html_start");
+extern const char root_end[] asm("_binary_root_html_end");
+
+extern const char success_start[] asm("_binary_success_html_start");
+extern const char success_end[] asm("_binary_success_html_end");
+static const char *TAG = "wifi station";
+
+
+//Wifi Variables
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_PSK //It is assumed that the wifi uses this security
+#define EXAMPLE_ESP_MAXIMUM_RETRY 5
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT      BIT1
+static EventGroupHandle_t s_wifi_event_group;
+static int s_retry_num = 0;
+
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+      esp_wifi_connect();
+  } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+      if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+          esp_wifi_connect();
+          s_retry_num++;
+      } else {
+          xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+      }
+  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+      s_retry_num = 0;
+      xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+  }
+}
+
+void comm_wifi_init(void)
+{
+  //Configuring wifi task
+  s_wifi_event_group = xEventGroupCreate();
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_netif_create_default_wifi_sta();
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+  esp_event_handler_instance_t instance_any_id;
+  esp_event_handler_instance_t instance_got_ip;
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                      ESP_EVENT_ANY_ID,
+                                                      &wifi_event_handler,
+                                                      NULL,
+                                                      &instance_any_id));
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                      IP_EVENT_STA_GOT_IP,
+                                                      &wifi_event_handler,
+                                                      NULL,
+                                                      &instance_got_ip));
+}
+void comm_wifi_start(uint8_t* inputSSID, uint8_t* inputPass)
+{
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = "",
+            .password = "",
+            .threshold.authmode = ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD,
+            .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+        },
+    };
+    memcpy(wifi_config.ap.ssid, inputSSID, 32);
+    memcpy(wifi_config.ap.password, inputPass, 32);
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    if (bits & WIFI_CONNECTED_BIT) {
+      //Successful wifi Connect
+        ESP_LOGI(TAG, "Connected");
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Connected");
+    }
+}
+
+// HTTP GET Handler
+static esp_err_t root_get_handler(httpd_req_t *req)
+{
+    const uint32_t root_len = root_end - root_start;
+
+    ESP_LOGI(TAG, "Serve root");
+    httpd_resp_set_type(req, "text/html");
+    httpd_resp_send(req, root_start, root_len);
+
+    return ESP_OK;
+}
+
+static esp_err_t success_get_handler(httpd_req_t *req)
+{
+    const uint32_t success_len = success_end - success_start;
+
+    ESP_LOGI(TAG, "Success page");
+    httpd_resp_set_type(req, "text/html");
+    httpd_resp_send(req, success_start, success_len);
+
+    return ESP_OK;
+}
+
+static const httpd_uri_t root = {
+    .uri = "/",
+    .method = HTTP_GET,
+    .handler = root_get_handler
+};
+
+static const httpd_uri_t success_page = {
+    .uri = "/success",
+    .method = HTTP_GET,
+    .handler = success_get_handler
+};
+
+// HTTP Error (404) Handler - Redirects all requests to the root page
+esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
+{
+    // Set status
+    httpd_resp_set_status(req, "302 Temporary Redirect");
+    // Redirect to the "/" root directory
+    httpd_resp_set_hdr(req, "Location", "/");
+    // iOS requires content in the response to detect a captive portal, simply redirecting is not sufficient.
+    httpd_resp_send(req, "Redirect to the captive portal", HTTPD_RESP_USE_STRLEN);
+
+    ESP_LOGI(TAG, "Redirecting to root");
+    return ESP_OK;
+}
+
+static httpd_handle_t start_webserver(void)
+{
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    config.max_open_sockets = 8;
+    config.lru_purge_enable = true;
+
+    // Start the httpd server
+    ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+    if (httpd_start(&server, &config) == ESP_OK) {
+        // Set URI handlers
+        ESP_LOGI(TAG, "Registering URI handlers");
+        httpd_register_uri_handler(server, &root);
+        httpd_register_uri_handler(server, &success_page);
+        httpd_register_err_handler(server, HTTPD_404_NOT_FOUND, http_404_error_handler);
+    }
+    return server;
+}
+
+
+void app_main(void)
+{
+    /*
+        Turn of warnings from HTTP server as redirecting traffic will yield
+        lots of invalid requests
+    */
+    esp_log_level_set("httpd_uri", ESP_LOG_ERROR);
+    esp_log_level_set("httpd_txrx", ESP_LOG_ERROR);
+    esp_log_level_set("httpd_parse", ESP_LOG_ERROR);
+    // Initialize NVS needed by Wi-Fi
+    ESP_ERROR_CHECK(nvs_flash_init());
+
+    // Initialise ESP32 in SoftAP mode
+    comm_wifi_init();
+    comm_wifi_start((uint8_t*)"SpectrumSetup-58", (uint8_t*)"fastcrown231");
+
+    // Start the server for the first time
+    static httpd_handle_t server = NULL;
+    server = start_webserver();
+    // Start the DNS server that will redirect all queries to the softAP IP
+    while(server) {
+        sleep(5);
+    }
+}
